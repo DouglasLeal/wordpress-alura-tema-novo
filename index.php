524924bb365d5c499972304e47871811a01221c5
Douglas Leal <douglas.leal@live.com>
2023-03-27 09:52:53 -0300

    mostrando texto metaboxes

--- index.php
+++ index.php
@@ -41,4 +41,30 @@ if ($query->have_posts()):
 endif;
 ?>
 
+<?php
+    $args = array(
+    'post_type' => 'banners',
+    'post_status' => 'publish',
+    'posts_per_page' => 1
+    );
+
+    $query = new WP_Query($args);
+    if($query->have_posts()):
+    while($query->have_posts()): $query->the_post();
+    ?>
+    <main>
+        <div class="imagem-banner">
+            <?php the_post_thumbnail(); ?>
+        </div>
+        <div class="texto-banner-dinamico">
+            <p id="texto-banner"><?php the_title(); ?></p>
+            <p id="texto-banner"><?= get_post_meta(get_the_ID(), "_texto_home_1", true); ?></p>
+            <p id="texto-banner"><?= get_post_meta(get_the_ID(), "_texto_home_2", true); ?></p>
+        </div>
+    </main>
+<?php
+endwhile;
+endif;
+?>
+
 <?php get_footer() ?>
